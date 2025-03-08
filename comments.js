5943999c50f28676e7b732fae316e1400d77d8f3
Estellise Yukihime <shiranoyukihime@gmail.com>
2025-03-08 11:45:34 +0000

    Copilot third commit 2

--- comments.js
+++ comments.js
@@ -1,26 +1,24 @@
-// create web server
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const port = 8080;
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// create server
-http.createServer((req, res) => {
-  const q = url.parse(req.url, true);
-  const filename = '.' + q.pathname;
-  fs.readFile(filename, (err, data) => {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end('404 Not Found');
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+// Set up the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-// run node comments.js
-// visit http://localhost:8080/comments.html
-// try to submit a comment
-// check the file comments.txt
+
+// Create a route
+app.get('/', (req, res) => {
+  res.send('Welcome to the homepage!');
+});
+
+// Create a route
+app.get('/comments', (req, res) => {
+  res.send('Welcome to the comments page!');
+});
+
+// Create a route
+app.get('/about', (req, res) => {
+  res.send('Welcome to the about page!');
+});
